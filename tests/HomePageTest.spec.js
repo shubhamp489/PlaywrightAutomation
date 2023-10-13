b0a299a0d2934fd80df6699f7fe6a0efef8f32e6
shubhamp489 <shubhamp489@gmail.com>
2023-10-13 21:03:15 +0530

    Added Playwright first test

---
+++ tests/HomePageTest.spec.js
@@ -0,0 +1,22 @@
+const {test , expect}  = require('@playwright/test');
+
+
+test('Home Page',async({page})=>{
+
+    await page.goto('https://demoblaze.com/index.html');
+   
+    const pageTitle = page.title();   //to capture title of the webpage
+    console.log('Page title is :' , pageTitle);    //to print title
+   
+    await expect(page).toHaveTitle('STORE');   //to validate page title
+    await expect(page).toHaveURL('https://demoblaze.com/index.html');    //to validate page url
+   
+    const pageUrl = page.url();
+    console.log('Page url is : ',pageUrl);
+    
+
+//testing
+    //to close page
+    await page.close();
+
+})
